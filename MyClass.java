9a28c3c3a1c3604829aba053f271d46a735674cc
Jenny <lanying99@yahoo.com>
2019-08-20 14:46:06 -0500

    Update MyClass.java

--- MyClass.java
+++ MyClass.java
@@ -1,5 +1,16 @@
 public class MyClass {
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-  }
-} 
+  public static void main(String[] args) {
+    int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
+    for (int i = 0; i < myNumbers.length; ++i) {
+	System.out.println(myNumbers[i][0]);
+	System.out.println("");
+      	
+ 	for(int j = 0; j < myNumbers[i].length; ++j) {
+        	System.out.println(myNumbers[i][j]);
+      	}
+    }
+  }
+}
+
+
+

44f883435bd2273240981f34bbf856ab52cdea2b
Maggie Y <maggie.yuan@gmail.com>
2019-08-20 19:40:27 -0500

    Create HelloMaggie.java

---
+++ HelloMaggie.java
@@ -0,0 +1,37 @@
+
+
+public class HelloMaggie {
+    public static void main(String[] args) {
+        String name = "Maggie Yuan";
+        String school = "Valley View Middle School";
+        System.out.println (name + ":"+ " " + school);
+
+        String txt = "Ellen Yuan: Normandale";
+        System.out.println (txt);
+
+        String firstname = "Michelle";
+        String lastname = "Jiang";
+        String newschool = "Blake";
+        System.out.println (firstname + " " + lastname + ":" + " " + newschool);
+
+        //new section
+
+       int x = 2;
+       float y = 6.88f;
+        int z = 30;
+        System.out.println (20 * x + y/z);
+
+
+
+
+            int num  = 10, M , total = 0;
+            for(M = 1; M <= 10; M++)
+                total = total + M;
+            System.out.println("10!:" +total);
+        }
+    }
+
+
+
+
+
